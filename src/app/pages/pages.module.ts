4b81b3beffe9f27172fb1be093d690aa1697afe4
sergio <smondragon4321@gmail.com>
2021-04-07 13:30:12 -0500

    Fin seccion 6

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+
+import { ChartsModule } from 'ng2-charts';
+
+//Modulos
+import { SharedModule } from '../shared/shared.module';
+import { ComponentsModule } from '../components/components.module';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
-import { SharedModule } from '../shared/shared.module';
+import { FormsModule } from '@angular/forms'
+
+
 
 //Componentes Duplicados
 
@@ -26,7 +35,10 @@ const PAGES_COMPONENT = [
   imports: [ 
     CommonModule,
     SharedModule,
-    RouterModule
+    ChartsModule,
+    RouterModule,
+    FormsModule,
+    ComponentsModule
   ]
 })
 export class PagesModule { }
